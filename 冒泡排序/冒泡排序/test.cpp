a514426ef88208be3dc88f811f15c4327619d3d3
SLShow <37412220+SLShow@users.noreply.github.com>
2018-03-16 19:29:32 +0800

    创建一个项目
    this is a test

---
+++ 冒泡排序/冒泡排序/test.cpp
@@ -0,0 +1,28 @@
+////////////////冒泡排序
+#include<stdio.h>
+#include<stdlib.h>
+
+const int N = 5;
+int a[101];
+
+int main() {
+	for (int i = 0;i < N;i++) {
+		scanf("%d", &a[i]);
+	}
+
+	int temp = 0;
+	for (int i = 0;i < N;i++) {
+		for (int j = 0;j < N-1-i;j++) {
+			if (a[j] > a[j + 1]) {//若前面大于后面，则交换，使最大的沉水底
+				temp = a[j + 1];
+				a[j + 1] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	
+	for (int i = 0;i < N;i++)
+		printf("%d ", a[i]);
+
+	return 0;
+}
